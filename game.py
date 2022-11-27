6f5d13b6de8449a48bcc017eba19f6d09682e12f
Ehsan138 <67409692+Ehsan138@users.noreply.github.com>
2022-11-26 17:57:22 -0800

    Add solutions for make_board and board_visual functions

--- game.py
+++ game.py
@@ -4,34 +4,34 @@ Michelle Kwok A01323329
 """
 
 
-def game():
-    rows = 10
-    columns = 10
-    board = make_board(rows, columns)
-    character = make_character("Player name")
-    achieved_goal = False
-    while not achieved_goal:
-        # Tell the user where they are describe_current_location(board, character)
-        direction = get_user_choice()
-        valid_move = validate_move(board, character, direction)
-        if valid_move:
-            move_character(character)
-            describe_current_location(board, character)
-            there_is_a_challenge = check_for_challenges()
-            if there_is_a_challenge:
-                execute_challenge_protocol(character)
-                if character_has_leveled():
-                    execute_glow_up_protocol()
-            achieved_goal = check_if_goal_attained(board, character)
-        else:
-            # Tell the user they can't go in that direction
-    # Print end of game (congratulations or sorry you died)
-
-
-    # create dictionary w tuples as key
-    # list as value, first thing string that describes location, second thing some event/character/question
-    # enumerated list to give users a selection of answers so they can type as little as possible
-    # picked from enumerated list if they want to go N, S, E, W: return true if they can move is valid, false if illegal
+# def game():
+#     rows = 10
+#     columns = 10
+#     board = make_board(rows, columns)
+#     character = make_character("Player name")
+#     achieved_goal = False
+#     while not achieved_goal:
+#         # Tell the user where they are describe_current_location(board, character)
+#         direction = get_user_choice()
+#         valid_move = validate_move(board, character, direction)
+#         if valid_move:
+#             move_character(character)
+#             describe_current_location(board, character)
+#             there_is_a_challenge = check_for_challenges()
+#             if there_is_a_challenge:
+#                 execute_challenge_protocol(character)
+#                 if character_has_leveled():
+#                     execute_glow_up_protocol()
+#             achieved_goal = check_if_goal_attained(board, character)
+#         else:
+#             # Tell the user they can't go in that direction
+#     # Print end of game (congratulations or sorry you died)
+#
+#
+#     # create dictionary w tuples as key
+#     # list as value, first thing string that describes location, second thing some event/character/question
+#     # enumerated list to give users a selection of answers so they can type as little as possible
+#     # picked from enumerated list if they want to go N, S, E, W: return true if they can move is valid, false if illegal
 
 def make_character(name):
     """
@@ -40,34 +40,89 @@ def make_character(name):
     :param name: a string
     :return: dictionary that contains key:value pairs
     """
-    character_dictionary = {"Name" : name, "X-coordinate": 0,
-                            "Y-coordinate": 0, "Current HP": 5, "Max HP": 5}
+    character_dictionary = {"Name": name, "x_coordinate": 0,
+                            "y_coordinate": 0, "Current HP": 5, "Max HP": 5}
 
     return character_dictionary
 
 
-def make_board():
+def make_board(rows, columns):
     board = {}
-    # for row in rows:
-    #     for column in columns:
-    #         board[(row, column)] = ["Welcome to the pit of doom", None]
-    board[(0, 0)] = adfkjdfklj
+    text = ''
+
+    for row in range(1, rows + 1):
+        text += '\n'
+
+        for column in range(1, columns + 1):
+            # board[(row, column)] = board(row, column)
+
+            # text += f"{row * column} \t"
+
+            board[(row, column)] = 'None'
+
+            # print('{:3}'.format(x * y), end=' ')
+            # board[(row, column)] = ["Welcome to the pit of doom", None]
+
+    # for key, value in board.items:
+    #     if value == 'Empty':
+
+    return board
 
 
-def describe_current_location():
+def board_visual(rows, columns):
+    """
+
+    :param rows:
+    :param columns:
+    :return:
+    """
+    board = make_board(rows, columns)
+    text = ''
+
+    # for key in board:
+    #     if key[0]
+
+    for row in range(1, rows + 1):
+        text += '\n'
+        for column in range(1, columns + 1):
+            text += f"{board[(row, column)]} \t"
+            # if (row, column) in board:
+
+    return text
 
 
-def get_user_choice():
 
 
-def validate_move():
+
+    # board[(0, 0)] = adfkjdfklj
+
+
+def board(coordinate_x , coordinate_y):
+    """
+
+    :param coordinate_x:
+    :param coordinate_y:
+    :return:
+    """
+
+    return 'Empty'
+
+
+# def describe_current_location():
+#
+#
+# def get_user_choice():
+#
+#
+# def validate_move():
 
 
 
 
 
 def main():
-    game()
+    print(make_board(10, 10))
+    print(board_visual(10, 10))
 
 
 if __name__ == "__main__":
