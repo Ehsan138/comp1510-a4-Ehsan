884dc759862ff06531ebb8a07fb8ce057aea813c
mimikwkk <113390005+mimikwkk@users.noreply.github.com>
2022-12-04 13:49:46 -0800

    Add unit test for make_character

---
+++ test_game_make_character.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+from unittest.mock import patch
+import game
+
+
+class TestMakeCharacter(TestCase):
+
+    @patch('builtins.input', side_effect=['Chris'])
+    def test_make_character_Chris(self, mock_input):
+        actual = game.make_character()
+        expected = {"Name": 'Chris', "x_coordinate": 1, "y_coordinate": 1, "Current_HP": 100,
+                                          "Max_HP": 100, "Experience_Points": 0, "Level": 1, "trivia_one": 0,
+                                          "trivia_two": 0, "trivia_three": 0, "trivia_four": 0, "trivia_five": 0,
+                                          "battle_one": 0, "battle_two": 0, "battle_three": 0, "battle_four": 0,
+                                          "battle_five": 0, "battle_six": 0, "battle_final": 0}
+        self.assertEqual(expected, actual)
+
+    @patch('builtins.input', side_effect=['Hoda'])
+    def test_make_character_Hoda(self, mock_input):
+        actual = game.make_character()
+        expected = {"Name": 'Hoda', "x_coordinate": 1, "y_coordinate": 1, "Current_HP": 100,
+                    "Max_HP": 100, "Experience_Points": 0, "Level": 1, "trivia_one": 0,
+                    "trivia_two": 0, "trivia_three": 0, "trivia_four": 0, "trivia_five": 0,
+                    "battle_one": 0, "battle_two": 0, "battle_three": 0, "battle_four": 0,
+                    "battle_five": 0, "battle_six": 0, "battle_final": 0}
+        self.assertEqual(expected, actual)
+
