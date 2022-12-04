c17400456f5d2d77ad6e4402e941433430d7385d
mimikwkk <113390005+mimikwkk@users.noreply.github.com>
2022-12-04 13:57:04 -0800

    Add unit test for is_alive

---
+++ test_game_is_alive.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+import game
+
+
+class TestIsAlive(TestCase):
+
+    def test_is_alive_Chris(self):
+        character_chris = {"Name": 'Chris', "x_coordinate": 1, "y_coordinate": 1, "Current_HP": 100, "Max_HP": 100}
+        actual = game.is_alive(character_chris)
+        expected = True
+        self.assertEqual(expected, actual)
+
+    def test_is_not_alive_Chris(self):
+        character_chris = {"Name": 'Chris', "x_coordinate": 1, "y_coordinate": 1, "Current_HP": 0, "Max_HP": 100}
+        actual = game.is_alive(character_chris)
+        expected = False
+        self.assertEqual(expected, actual)
